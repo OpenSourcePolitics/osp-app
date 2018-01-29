3a8a5cf4377690f472fa28649146c0732acea494
Julie Simon <juliesimon.js@gmail.com>
2018-01-29 17:36:40 +0100

    run bundle update + importe migrations + rake db migrate + delete migration that is present twice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125101205) do
+ActiveRecord::Schema.define(version: 20180126130738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
