884fb12b5a2595dede9e9248764b6a57324e06a9
moustachu <git@moustachu.net>
2018-02-09 00:41:10 +0100

    better image size

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -8,6 +8,8 @@ Decidim.configure do |config|
   config.default_locale = :en
   config.available_locales = [:en, :fr]
 
+  config.maximum_attachment_size = 20.megabytes
+
   # Geocoder configuration
   # config.geocoder = {
   #   static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
