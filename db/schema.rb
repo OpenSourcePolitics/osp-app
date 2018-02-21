6a7fd4ffb9f0163163006c71f587f5b3d86de1b1
mako <mako@osp.cat>
2018-02-21 04:08:49 +0100

    update on prod ;)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202222152) do
+ActiveRecord::Schema.define(version: 20180221025839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,42 @@ ActiveRecord::Schema.define(version: 20180202222152) do
     t.datetime "updated_at", null: false
     t.index ["decidim_accountability_result_id"], name: "index_decidim_accountability_timeline_entries_on_results_id"
     t.index ["entry_date"], name: "index_decidim_accountability_timeline_entries_on_entry_date"
+  end
+
+  create_table "decidim_assemblies", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.string "hashtag"
+    t.integer "decidim_organization_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.jsonb "title", null: false
+    t.jsonb "subtitle", null: false
+    t.jsonb "short_description", null: false
+    t.jsonb "description", null: false
+    t.string "hero_image"
+    t.string "banner_image"
+    t.boolean "promoted", default: false
+    t.datetime "published_at"
+    t.jsonb "developer_group"
+    t.jsonb "meta_scope"
+    t.jsonb "local_area"
+    t.jsonb "target"
+    t.jsonb "participatory_scope"
+    t.jsonb "participatory_structure"
+    t.boolean "show_statistics", default: false
+    t.integer "decidim_scope_id"
+    t.boolean "scopes_enabled", default: true, null: false
+    t.index ["decidim_organization_id", "slug"], name: "index_unique_assembly_slug_and_organization", unique: true
+    t.index ["decidim_organization_id"], name: "index_decidim_assemblies_on_decidim_organization_id"
+  end
+
+  create_table "decidim_assembly_user_roles", force: :cascade do |t|
+    t.integer "decidim_user_id"
+    t.integer "decidim_assembly_id"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["decidim_assembly_id", "decidim_user_id", "role"], name: "index_unique_user_and_assembly_role", unique: true
   end
 
   create_table "decidim_attachments", id: :serial, force: :cascade do |t|
@@ -318,6 +354,15 @@ ActiveRecord::Schema.define(version: 20180202222152) do
     t.index ["report_count"], name: "decidim_moderations_report_count"
   end
 
+  create_table "decidim_navbar_links", force: :cascade do |t|
+    t.bigint "decidim_organization_id"
+    t.jsonb "title"
+    t.string "link"
+    t.string "target"
+    t.integer "weight"
+    t.index ["decidim_organization_id"], name: "index_decidim_navbar_links_on_decidim_organization_id"
+  end
+
   create_table "decidim_newsletters", id: :serial, force: :cascade do |t|
     t.jsonb "subject"
     t.jsonb "body"
@@ -392,44 +437,6 @@ ActiveRecord::Schema.define(version: 20180202222152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["decidim_feature_id"], name: "index_decidim_pages_pages_on_decidim_feature_id"
-  end
-
-  create_table "decidim_participations_participation_votes", id: :serial, force: :cascade do |t|
-    t.integer "decidim_participation_id", null: false
-    t.integer "decidim_author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["decidim_author_id"], name: "decidim_participations_vote_author"
-    t.index ["decidim_participation_id", "decidim_author_id"], name: "decidim_participations_vote_participation_author_unique", unique: true
-    t.index ["decidim_participation_id"], name: "decidim_participations_vote_participation"
-  end
-
-  create_table "decidim_participations_participations", id: :serial, force: :cascade do |t|
-    t.string "title"
-    t.text "body", null: false
-    t.integer "decidim_feature_id", null: false
-    t.integer "decidim_author_id"
-    t.integer "decidim_scope_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "participation_votes_count", default: 0, null: false
-    t.integer "decidim_user_group_id"
-    t.string "state"
-    t.datetime "answered_at"
-    t.jsonb "answer"
-    t.string "reference"
-    t.text "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "participation_type"
-    t.index ["body"], name: "decidim_participations_participation_body_search"
-    t.index ["created_at"], name: "index_decidim_participations_participations_on_created_at"
-    t.index ["decidim_author_id"], name: "index_decidim_participations_on_decidim_author_id"
-    t.index ["decidim_feature_id"], name: "index_decidim_participations_on_decidim_feature_id"
-    t.index ["decidim_scope_id"], name: "index_decidim_participations_on_decidim_scope_id"
-    t.index ["participation_votes_count"], name: "decidim_participations_votes_count"
-    t.index ["state"], name: "index_decidim_participations_participations_on_state"
-    t.index ["title"], name: "decidim_participations_participation_title_search"
   end
 
   create_table "decidim_participatory_process_groups", id: :serial, force: :cascade do |t|
