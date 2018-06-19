c366921ac8aaa6fde0737ea27082dbdad57fb671
Julie Simon <juliesimon.js@gmail.com>
2018-06-19 18:49:15 +0200

    Add Nancy authorization handler

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  Decidim::Verifications.register_workflow(:nancy_authorization_handler) do |auth|
+    auth.form = "Decidim::NancyAuthorizationHandler"
+  end
 end
