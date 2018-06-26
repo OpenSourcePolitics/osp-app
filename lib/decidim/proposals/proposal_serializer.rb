f26b9ae7c2229cbbcebb1fbb5c15ac869765290d
Julie Simon <juliesimon.js@gmail.com>
2018-06-26 17:50:29 +0200

    Add author_id, published_at and hidden? to proposal exports

---
+++ lib/decidim/proposals/proposal_serializer.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Proposals
+    # This class serializes a Proposal so can be exported to CSV, JSON or other
+    # formats.
+    class ProposalSerializer < Decidim::Exporters::Serializer
+      include Decidim::ResourceHelper
+
+      # Public: Initializes the serializer with a proposal.
+      def initialize(proposal)
+        @proposal = proposal
+      end
+
+      # Public: Exports a hash with the serialized data for this proposal.
+      def serialize
+        {
+          id: @proposal.id,
+          category: {
+            id: @proposal.category.try(:id),
+            name: @proposal.category.try(:name)
+          },
+          scope: {
+            id: @proposal.scope.try(:id),
+            name: @proposal.scope.try(:name)
+          },
+          author_id: @proposal.author.id,
+          title: @proposal.title,
+          body: @proposal.body,
+          votes: @proposal.proposal_votes_count,
+          comments: @proposal.comments.count,
+          created_at: @proposal.created_at,
+          published_at: @proposal.published_at,
+          hidden: @proposal.hidden?,
+          url: url,
+          feature: { id: feature.id },
+          meeting_urls: meetings
+        }
+      end
+
+      private
+
+      attr_reader :proposal
+
+      def feature
+        proposal.feature
+      end
+
+      def meetings
+        @proposal.linked_resources(:meetings, "proposals_from_meeting").map do |meeting|
+          Decidim::ResourceLocatorPresenter.new(meeting).url
+        end
+      end
+
+      def url
+        Decidim::ResourceLocatorPresenter.new(proposal).url
+      end
+    end
+  end
+end
