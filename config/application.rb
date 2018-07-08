70547d4a7375d5db54a72a6d2948829928603fa3
moustachu <git@moustachu.net>
2018-07-08 21:00:13 +0200

    fix Timezone

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,18 @@ module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = "Paris"
+
+    if ENV["ASSET_HOST"].present?
+      if !ENV["HEROKU_APP_NAME"].present?
+        if Rails.env.production?
+          protocol = 'https'
+        else
+          protocol = 'http'
+        end
+        config.action_mailer.asset_host = "#{protocol}://#{ENV['ASSET_HOST']}"
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
