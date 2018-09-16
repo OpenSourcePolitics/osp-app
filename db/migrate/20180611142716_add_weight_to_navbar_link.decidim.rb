590c64fd749c3b640ce9872a7ef978b1a0a8da55
moustachu <git@moustachu.net>
2018-09-16 23:50:30 +0200

    :package: :rocket: migrations sync

--- db/migrate/20180611142716_add_weight_to_navbar_link.decidim.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from decidim (originally 20180112110646)
-class AddWeightToNavbarLink < ActiveRecord::Migration[5.1]
-  def change
-    add_column :decidim_navbar_links, :weight, :integer
-  end
-end
