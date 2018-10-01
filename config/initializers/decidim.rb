f7fb346ae5ebce51ab7237a8743623479478d475
moustachu <git@moustachu.net>
2018-10-02 00:02:08 +0200

    :globe_with_meridians: Add DE

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -7,7 +7,7 @@ Decidim.configure do |config|
 
   # Change these lines to set your preferred locales
   config.default_locale = :fr
-  config.available_locales = [:en, :ca, :es, :fr, :nl]
+  config.available_locales = [:en, :ca, :es, :fr, :nl, :de]
 
   # Geocoder configuration
   config.geocoder = {
