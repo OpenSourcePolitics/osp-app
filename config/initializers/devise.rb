a547e9386b83dd5ff9c693d0fec7332186bbfafb
moustachu <git@moustachu.net>
2018-11-08 09:50:05 +0100

    :bug: missing files ... git ...

---
+++ config/initializers/devise.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+ # Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
+ Devise.setup do |config|
+  # ==> Configuration for :invitable
+  # The period the generated invitation token is valid, after
+  # this period, the invited resource won't be able to accept the invitation.
+  # When invite_for is 0 (the default), the invitation won't expire.
+  config.invite_for = 0
+end
