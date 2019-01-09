0d137d2d403e8d800f7038424e4f67546ebc125a
moustachu <git@moustachu.net>
2019-01-09 20:12:24 +0100

    :lipstick: UI for roubaix

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module DecidimApp
 
     # This needs to be set for correct images URLs in emails
     # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
-    # config.action_mailer.asset_host = "https://your.server.url"
+    config.action_mailer.asset_host = "https://budgetparticipatif.ville-roubaix.fr"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
