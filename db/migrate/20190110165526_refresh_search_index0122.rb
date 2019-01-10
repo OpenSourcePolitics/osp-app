2c986ac6d6cddb3c6b74c612db5200dc4565a453
moustachu <git@moustachu.net>
2019-01-10 18:37:58 +0100

    :bug: RC fixes

---
+++ db/migrate/20190110165526_refresh_search_index0122.rb
@@ -0,0 +1,18 @@
+class RefreshSearchIndex0122 < ActiveRecord::Migration[5.2]
+  def change
+
+    # From decidim-core/db/migrate/20180206143340_fix_reference_for_all_resources.rb
+    models = ActiveRecord::Base.descendants.select { |c| c.included_modules.include?(Decidim::HasReference) }
+
+    models.each do |model|
+      model.find_each(&:touch)
+    end
+
+    # From decidim-core/lib/decidim/searchable.rb
+    Decidim::Proposals::Proposal.all.each do |p|
+      p.try_add_to_index_as_search_resource
+      p.try_update_index_for_search_resource
+    end
+
+  end
+end
