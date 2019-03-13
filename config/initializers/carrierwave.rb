46657ff845149c6be8b201adf57be96e412be60a
moustachu <git@moustachu.net>
2019-03-13 17:22:04 +0100

    :lipstick: NEC UI tweaks

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
   config.enable_processing = !Rails.env.test?
   # This needs to be set for correct attachment file URLs in emails
   # DON'T FORGET to ALSO set this in `config/application.rb`
-  # config.asset_host = "https://your.server.url"
+  config.asset_host = "https://numerique-en-commun.fr"
 end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
