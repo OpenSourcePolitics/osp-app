d30459cfd18e5cf4e7733f3be1f255561a30a6f9
Armand <fardeauarmand@gmail.com>
2019-05-09 15:33:08 +0200

    add sidekiq scheduler to sidekiq webview

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
 require "sidekiq/web"
+require "sidekiq-scheduler/web"
+
 Rails.application.routes.draw do
 
-  authenticate :user, lambda { |u| u.roles.include?("admin") } do
-    mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, lambda(&:admin?) do
+    mount Sidekiq::Web, at: "sidekiq", as: :sidekiq
   end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  mount Decidim::Core::Engine => '/'
+  mount Decidim::Core::Engine => "/"
   # mount Decidim::Map::Engine => '/map'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
