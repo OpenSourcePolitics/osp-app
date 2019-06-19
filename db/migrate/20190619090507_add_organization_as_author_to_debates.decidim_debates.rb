ea2fb7313c622de2b44508fc2a6e12a0457ee819
moustachu <git@moustachu.net>
2019-06-19 15:20:54 +0200

    fix migrations

--- db/migrate/20190619090507_add_organization_as_author_to_debates.decidim_debates.rb
+++ db/migrate/20190619090507_add_organization_as_author_to_debates.decidim_debates.rb
@@ -16,12 +16,11 @@ class AddOrganizationAsAuthorToDebates < ActiveRecord::Migration[5.2]
     Debate.find_each do |debate|
       if debate.decidim_author_id.present?
         debate.decidim_author_type = "Decidim::UserBaseEntity"
-        debate.save!
-      elsif debate.try(:organization).try(:id)
-        debate.decidim_author_id = debate.organization.id
+      else
+        debate.decidim_author_id = 5
         debate.decidim_author_type = "Decidim::Organization"
-        debate.save!
       end
+      debate.save!
     end
 
     add_index :decidim_debates_debates,
