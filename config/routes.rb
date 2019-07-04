c8a72f166f66dc1ecce774316d117aff888db007
Armand <fardeauarmand@gmail.com>
2019-07-04 15:55:01 +0200

    Added Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
   end
 
   mount Decidim::Core::Engine => '/'
