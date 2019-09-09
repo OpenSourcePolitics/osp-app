52c386603f3412e887fb213668c3baa07b47d3c3
moustachu <git@moustachu.net>
2019-09-09 18:53:30 +0200

    :globe_with_meridians: FR change "Initiatives" to "Pétitions"

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module DecidimAws
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = "Europe/Paris"
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
