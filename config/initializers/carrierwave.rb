32c8a68a72474e0bd48a98252d07b451839b7459
Armand <fardeauarmand@gmail.com>
2019-10-24 11:32:14 +0200

    Use scaleway bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,15 @@
 
 # Default CarrierWave setup.
 #
-CarrierWave.configure do |config|
-  config.permissions = 0o666
-  config.directory_permissions = 0o777
-  config.storage = :file
-  config.enable_processing = !Rails.env.test?
-  # This needs to be set for correct attachment file URLs in emails
-  # DON'T FORGET to ALSO set this in `config/application.rb`
-  # config.asset_host = "https://your.server.url"
-end
+# CarrierWave.configure do |config|
+#   config.permissions = 0o666
+#   config.directory_permissions = 0o777
+#   config.storage = :file
+#   config.enable_processing = !Rails.env.test?
+#   # This needs to be set for correct attachment file URLs in emails
+#   # DON'T FORGET to ALSO set this in `config/application.rb`
+#   # config.asset_host = "https://your.server.url"
+# end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
 if ENV["HEROKU_APP_NAME"].present?
@@ -31,4 +31,25 @@ if ENV["HEROKU_APP_NAME"].present?
       config.storage = :fog
     end
   end
+end
+
+#if Rails.application.secrets.scaleway.id.present? && Rails.env.production?
+require "carrierwave/storage/fog"
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.scaleway.id,
+      aws_secret_access_key: Rails.application.secrets.scaleway.token,
+      region: "fr-par",
+      endpoint: "https://s3.fr-par.scw.cloud"
+  }
+  config.fog_directory = Rails.application.secrets.scaleway.fetch(:butcket_name) { "osp_app" }
+  config.fog_attributes = {
+      'Cache-Control' => "max-age=#{365.day.to_i}",
+      'X-Content-Type-Options' => "nosniff"
+  }
+  #end
 end
