09f9b760053f9e31331bcd4b233aadee2f498907
moustachu <git@moustachu.net>
2019-10-25 10:22:08 +0200

    :wrench: fix configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 # Default CarrierWave setup.
-#
-CarrierWave.configure do |config|
-  config.permissions = 0o666
-  config.directory_permissions = 0o777
-  config.storage = :file
-  config.enable_processing = !Rails.env.test?
-  # This needs to be set for correct attachment file URLs in emails
-  # DON'T FORGET to ALSO set this in `config/application.rb`
-  # config.asset_host = "https://your.server.url"
-end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
 # if ENV["HEROKU_APP_NAME"].present?
@@ -33,19 +23,31 @@ end
 #   end
 # end
 
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: Rails.application.secrets.scaleway.dig(:id),
-#       aws_secret_access_key: Rails.application.secrets.scaleway.dig(:token),
-#       region: "fr-par",
-#       endpoint: "https://s3.fr-par.scw.cloud"
-#   }
-#   config.fog_directory = Rails.application.secrets.scaleway.fetch(:bucket_name) { "osp_app" }
-#   config.fog_attributes = {
-#       'Cache-Control' => "max-age=#{365.day.to_i}",
-#       'X-Content-Type-Options' => "nosniff"
-#   }
-# end
+if Rails.application.secrets.dig(:scaleway, :id).present? && Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: Rails.application.secrets.dig(:scaleway, :id),
+        aws_secret_access_key: Rails.application.secrets.dig(:scaleway, :token),
+        region: "fr-par",
+        endpoint: "https://s3.fr-par.scw.cloud"
+    }
+    config.storage = :fog
+    config.fog_directory = Rails.application.secrets.dig(:scaleway, :bucket_name) { "ccc-bucket" }
+    config.fog_attributes = {
+        'Cache-Control' => "max-age=#{365.day.to_i}",
+        'X-Content-Type-Options' => "nosniff"
+    }
+  end
+else
+  CarrierWave.configure do |config|
+    config.permissions = 0o666
+    config.directory_permissions = 0o777
+    config.storage = :file
+    config.enable_processing = !Rails.env.test?
+    # This needs to be set for correct attachment file URLs in emails
+    # DON'T FORGET to ALSO set this in `config/application.rb`
+    # config.asset_host = "https://your.server.url"
+  end
+end
