14c33f14bfde2049a8ab5c1af4be3c4f4422bd46
moustachu <git@moustachu.net>
2019-11-04 21:31:37 +0100

    restore header snippet

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,2 +1,4 @@
 # require 'extends/confirmations_controller_extend'
 require 'extends/initiative_model_extend'
+require "extends/organization_appearance_form_extend"
+require "extends/update_organization_appearance_extend"
