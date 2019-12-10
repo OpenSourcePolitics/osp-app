a558e4c02f4a0d5e6b07c54bd3b3f892a7dab637
moustachu <git@moustachu.net>
2019-12-10 08:55:26 +0100

    bump

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,4 +1,5 @@
 # require 'extends/confirmations_controller_extend'
-require 'extends/initiative_model_extend'
+require "extends/initiative_model_extend"
 require "extends/organization_appearance_form_extend"
 require "extends/update_organization_appearance_extend"
+require "extends/account_form_extend"
