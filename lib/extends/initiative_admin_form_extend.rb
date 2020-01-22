acad6faea5a3a1244de2f6d9ae7f9057b7ad6fb2
moustachu <git@moustachu.net>
2020-01-22 23:23:47 +0100

    fix initiatives admin form

--- lib/extends/initiative_admin_form_extend.rb
+++ lib/extends/initiative_admin_form_extend.rb
@@ -8,13 +8,6 @@ module InitiativeAdminFormExtend
 
     validate :title, :title_max_length
 
-    def signature_type_updatable?
-      @signature_type_updatable ||= begin
-                                      state ||= context.initiative.state
-                                      state == "validating" && context.current_user.admin?
-                                    end
-    end
-
     def title_max_length
       title.each do |locale, value|
         if value.length > 150
