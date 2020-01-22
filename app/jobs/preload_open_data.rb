a3a5899849ba81a6efdffa88266b4819fd0264d8
moustachu <git@moustachu.net>
2020-01-22 17:01:41 +0100

    fix scheduler

--- app/jobs/preload_open_data.rb
+++ app/jobs/preload_open_data.rb
@@ -3,9 +3,6 @@
 class PreloadOpenDataJob < ApplicationJob
 
   def perform
-    application_name = Rails.application.class.parent_name
-    application = Object.const_get(application_name)
-    application::Application.load_tasks
-    Rake::Task["decidim:open_data:export"].invoke
+    system "rake decidim:open_data:export"
   end
 end
