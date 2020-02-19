b4367bd4dc3933a05c8146f72b8a52be99ce30b6
moustachu <git@moustachu.net>
2020-02-19 20:54:07 +0100

    Add Action Authorizer

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -5,6 +5,15 @@ Decidim::Verifications.unregister_workflow(:id_documents)
 Decidim::Verifications.unregister_workflow(:postal_letter)
 Decidim::Verifications.unregister_workflow(:sms)
 
+
+# Decidim::Verifications.register_workflow(:decidim) do |workflow|
+#   workflow.engine = Decidim::Verifications::Omniauth::Engine
+#   workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
+#   workflow.omniauth_provider = :decidim
+#   # workflow.anti_affinity = [:decidim]
+#   # workflow.minimum_age = 18
+# end
+
 Decidim::Verifications.register_workflow(:cas) do |workflow|
   workflow.engine = Decidim::Verifications::Omniauth::Engine
   workflow.admin_engine = Decidim::Verifications::Omniauth::AdminEngine
---
+++ lib/decidim/verifications/omniauth/ubx_action_authorizer.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Verifications
+    module Omniauth
+      class UbxActionAuthorizer < Decidim::Verifications::DefaultActionAuthorizer
+        # Overrides the parent class method, but it still uses it to keep the base behavior
+        def authorize
+
+          status_code, data = *super
+
+          if status_code == :ok && authorization.metadata["status"] != "student"
+            status_code = :unauthorized
+          end
+
+          [status_code, data]
+        end
+      end
+    end
+  end
+end
