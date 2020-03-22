4e1fe392905c59d7b51c60371ae642806fa14251
moustachu <moustachu@osp.cat>
2020-03-22 23:26:32 +0100

    System security policy

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -9,6 +9,11 @@ Decidim.configure do |config|
   config.default_locale = :fr
   config.available_locales = [:fr]
 
+  # Restrict access to the system part with an authorized ip list.
+  # You can use a single ip like ("1.2.3.4"), or an ip subnet like ("1.2.3.4/24")
+  # You may specify multiple ip in an array ["1.2.3.4", "1.2.3.4/24"]
+  config.system_whitelist_ips = ["0.0.0.0/0"]
+
   # Geocoder configuration
   config.geocoder = {
       static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
