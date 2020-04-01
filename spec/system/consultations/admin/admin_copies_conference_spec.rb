d7548b766fe5dc7c3b693bfe6a1ffe750a043722
Armand Fardeau <fardeauarmand@gmail.com>
2020-04-01 13:45:31 +0200

    fix consultations

--- spec/system/consultations/admin/admin_copies_conference_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-describe "Admin copies conference", type: :system do
-  include_context "when admin administrating a conference"
-
-  it_behaves_like "copy conferences"
-end
