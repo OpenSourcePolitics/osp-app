db565b4cec16bd5433c52900e415a0277f4750d6
moustachu <moustachu@osp.cat>
2020-05-04 22:25:16 +0200

    FC login patch

--- lib/extends/create_omniauth_registration_extend.rb
+++ lib/extends/create_omniauth_registration_extend.rb
@@ -39,27 +39,47 @@ module CreateOmniauthRegistrationExtend
     def create_or_find_user
       generated_password = SecureRandom.hex
 
-      @user = Decidim::User.find_or_initialize_by(
-        email: verified_email,
-        organization: organization
-      )
+      if (verified_email || form.email).blank?
 
-      if !@user.persisted? || @user.invited_to_sign_up?
-        @user.email = (verified_email || form.email)
-        @user.name = form.name
-        @user.nickname = form.normalized_nickname
-        @user.newsletter_notifications_at = nil
-        @user.email_on_notification = true
-        @user.password = generated_password
-        @user.password_confirmation = generated_password
+        @user = Decidim::User.new(
+          email: "",
+          organization: organization,
+          name: form.name,
+          nickname: form.nickname,
+          newsletter_notifications_at: nil,
+          email_on_notification: false,
+          accepted_tos_version: organization.tos_version,
+          # managed: true,
+          password: generated_password,
+          password_confirmation: generated_password
+        )
+        @user.skip_confirmation!
 
-        @user.full_address = form.full_address
-        @user.custom_agreement_at = DateTime.now if form.custom_agreement
+      else
 
-        # TODO: raise ActiveRecord::RecordInvalid because of quality setting on uploader, this line is a quick fix
-        @user.remote_avatar_url = form.avatar_url if form.avatar_url.present? && !form.avatar_url.end_with?("svg")
-        @user.skip_confirmation! if verified_email
-        @user.accept_invitation if @user.invited_to_sign_up?
+        @user = Decidim::User.find_or_initialize_by(
+          email: verified_email,
+          organization: organization
+        )
+
+        if !@user.persisted? || @user.invited_to_sign_up?
+          @user.email = (verified_email || form.email)
+          @user.name = form.name
+          @user.nickname = form.normalized_nickname
+          @user.newsletter_notifications_at = nil
+          @user.email_on_notification = true
+          @user.password = generated_password
+          @user.password_confirmation = generated_password
+
+          @user.full_address = form.full_address
+          @user.custom_agreement_at = DateTime.now if form.custom_agreement
+
+          # TODO: raise ActiveRecord::RecordInvalid because of quality setting on uploader, this line is a quick fix
+          @user.remote_avatar_url = form.avatar_url if form.avatar_url.present? && !form.avatar_url.end_with?("svg")
+          @user.skip_confirmation! if verified_email # unless verified_email != form.email
+          @user.accept_invitation if @user.invited_to_sign_up?
+          @after_confirmation = (verified_email != form.email)
+        end
       end
 
       @user.tos_agreement = "1"
