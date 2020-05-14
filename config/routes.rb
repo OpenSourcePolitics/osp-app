5d89fbbca426b33520d37e901e5c7b1da8b4f342
Armand Fardeau <fardeauarmand@gmail.com>
2020-05-14 20:37:53 +0200

    Add admin sign in alias route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     # mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  devise_scope :user do
+    get '/admin_sign_in', to: "decidim/devise/sessions#new"
+  end
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   mount Decidim::Core::Engine => '/'
