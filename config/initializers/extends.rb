3398e02bac22836d697e5f2f8dbda956de2c4630
moustachu <moustachu@osp.cat>
2020-06-12 18:01:43 +0200

    :bug: miscellaneous fixes

--- config/initializers/extends.rb
+++ config/initializers/extends.rb
@@ -1,8 +1,8 @@
 # require 'extends/confirmations_controller_extend'
 require "extends/user_model_extend"
-if defined?(Decidim::Initiatives)
-  require "extends/initiative_model_extend"
-end
+# if defined?(Decidim::Initiatives)
+#   require "extends/initiative_model_extend"
+# end
 require "extends/account_form_extend"
 require "extends/omniauth_registration_form_extend"
 if defined?(Decidim::Initiatives)
@@ -10,6 +10,6 @@ if defined?(Decidim::Initiatives)
 end
 require "extends/organization_appearance_form_extend"
 require "extends/update_organization_appearance_extend"
-require "extends/destroy_account_extend"
+# require "extends/destroy_account_extend"
 require "extends/create_omniauth_registration_extend"
 require "extends/update_account_extend"
