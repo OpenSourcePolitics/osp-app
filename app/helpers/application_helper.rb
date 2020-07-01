986260ef46623c91f519c160463208036e47741c
quentinchampenois <quentin.champenois7@gmail.com>
2020-07-01 14:56:20 +0200

    refactor omniauth button modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def allowed_omniauth?(provider_name)
+    return true if request.env['PATH_INFO'].end_with?('/users/sign_in')
+    return true if request.env['PATH_INFO'].end_with?('/committee_requests/new')
+
+    (available_authorizations? && request.env.dig(:available_authorizations).include?(provider_name.to_s))
+  end
+
+  private
+
+  def available_authorizations?
+    !(request.env.dig(:available_authorizations).nil? || request.env.dig(:available_authorizations).empty?)
+  end
 end
