55d3fe631350906219803e6d0b68365be5766811
quentinchampenois <quentin.champenois7@gmail.com>
2020-07-01 18:01:03 +0200

    restrict geocoding search to France

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -16,7 +16,8 @@ Decidim.configure do |config|
   if !Rails.application.secrets.geocoder[:here_api_key].blank?
     config.geocoder = {
         static_map_url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview",
-        here_api_key: Rails.application.secrets.geocoder[:here_api_key]
+        here_api_key: Rails.application.secrets.geocoder[:here_api_key],
+        country_restriction: 'France'
     }
   end
 
