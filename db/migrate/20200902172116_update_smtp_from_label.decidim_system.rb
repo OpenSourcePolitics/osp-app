afa4eefff088a1b393a83d97e143a801eb624a24
moustachu <moustachu@osp.cat>
2020-09-02 23:39:20 +0200

    fix migration

--- db/migrate/20200902172116_update_smtp_from_label.decidim_system.rb
+++ db/migrate/20200902172116_update_smtp_from_label.decidim_system.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
 # This migration comes from decidim_system (originally 20200902171325)
+
 class UpdateSmtpFromLabel < ActiveRecord::Migration[5.2]
   def change
     Decidim::Organization.all.each do |org|
-      if org.smtp_settings[:from_email].blank?
-        org.smtp_settings[:from_email] = org.smtp_settings[:from]
+      if org.smtp_settings["from_email"].blank?
+        org.smtp_settings["from_email"] = org.smtp_settings["from"]
         org.save!
       end
     end
