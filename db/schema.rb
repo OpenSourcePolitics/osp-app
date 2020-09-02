1e693d1d67a36e0d4a58d6abb2cd0547a1b03c83
moustachu <moustachu@osp.cat>
2020-09-02 22:16:48 +0200

    Bump version for SMTP label

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_173027) do
+ActiveRecord::Schema.define(version: 2020_09_02_172116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
