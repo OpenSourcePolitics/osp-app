e2e310a1654b79b407a1c1ac6f9d6c7050eec937
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-09-08 13:31:34 +0200

    add activerecord-session_store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_153643) do
+ActiveRecord::Schema.define(version: 2020_09_08_112637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1336,6 +1336,15 @@ ActiveRecord::Schema.define(version: 2020_01_28_153643) do
     t.boolean "confidential", default: true, null: false
     t.index ["decidim_organization_id"], name: "index_oauth_applications_on_decidim_organization_id"
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "versions", force: :cascade do |t|
