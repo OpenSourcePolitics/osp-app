db276a7ea090df9cf5b11b22eca1cabe4574c622
Armand Fardeau <fardeauarmand@gmail.com>
2020-09-08 12:45:21 +0200

    Fix missing translations

--- config/initializers/decidim_verifications.rb
+++ config/initializers/decidim_verifications.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-# Decidim::Verifications.register_workflow(:dummy_authorization_handler) do |workflow|
-#   workflow.form = "DummyAuthorizationHandler"
-#   workflow.action_authorizer = "DummyAuthorizationHandler::ActionAuthorizer"
-#   workflow.expires_in = 1.hour
-#
-#   workflow.options do |options|
-#     options.attribute :postal_code, type: :string, default: "08001", required: false
-#   end
-# end
+Decidim::Verifications.register_workflow(:dummy_authorization_handler) do |workflow|
+  workflow.form = "DummyAuthorizationHandler"
+  workflow.action_authorizer = "DummyAuthorizationHandler::DummyActionAuthorizer"
+  workflow.expires_in = 1.month
+  workflow.renewable = true
+  workflow.time_between_renewals = 5.minutes
+end
 #
 # Decidim::Verifications.register_workflow(:another_dummy_authorization_handler) do |workflow|
 #   workflow.form = "AnotherDummyAuthorizationHandler"
