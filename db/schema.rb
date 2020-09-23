ba17c202e4fed4ad37a5b75722576f7f817dfe79
MoretS <s.moret@ecole-ipssi.net>
2020-09-23 18:45:06 +0200

    Delete duplicate and empty migrations + run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_121865) do
+ActiveRecord::Schema.define(version: 2020_09_02_160808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
