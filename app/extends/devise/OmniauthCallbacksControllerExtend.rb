9c9dfd6d59966526015ca22088f45e2de05d3d15
moustachu <git@moustachu.net>
2020-10-08 15:29:10 +0200

    skip verify_authenticity_token for SAML callbacks

---
+++ app/extends/devise/OmniauthCallbacksControllerExtend.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Devise::OmniauthCallbacksController.class_eval
+  skip_before_action :verify_authenticity_token, if: :saml_callback?
+
+  def saml_callback?
+    request.path.end_with?("users/auth/nyc/callback")
+  end
+end
