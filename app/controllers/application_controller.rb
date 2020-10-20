228a5df78f3817ec186e8f00e176c1c9a64f77d4
moustachu <git@moustachu.net>
2020-10-20 19:39:33 +0200

    add post_logout_callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  skip_before_filter :verify_authenticity_token, :only => [:post_logout_callback]
+
   def post_logout_callback
     redirect_to "/"
   end
