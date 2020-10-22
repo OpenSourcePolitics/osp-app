3a93cc74152a48982c1fd5657911af05ad677290
MoretS <s.moret@ecole-ipssi.net>
2020-10-22 15:22:54 +0200

    Enable navbar_links and term_custo modules + run navbar_links migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_124097) do
+ActiveRecord::Schema.define(version: 2020_10_22_132105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -902,6 +902,17 @@ ActiveRecord::Schema.define(version: 2020_10_22_124097) do
     t.string "target"
     t.integer "weight"
     t.index ["decidim_organization_id"], name: "index_decidim_navbar_links_on_decidim_organization_id"
+  end
+
+  create_table "decidim_navbar_links_navbar_links", force: :cascade do |t|
+    t.bigint "decidim_organization_id"
+    t.jsonb "title"
+    t.string "link"
+    t.string "target"
+    t.integer "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["decidim_organization_id"], name: "decidim_navbar_links_on_organization_id"
   end
 
   create_table "decidim_newsletters", id: :serial, force: :cascade do |t|
