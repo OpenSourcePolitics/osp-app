0cec00c538e50e3cb57b3dd9a5fedb8429f25cf9
Armand Fardeau <fardeauarmand@gmail.com>
2020-11-06 16:58:29 +0100

    Add a second beacon to user-side

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def render_helpscout_beacon(side)
+    return unless helpscout_beacon_id(side).present?
+
+    render_beacon(side, :main) unless helpscout_beacon_id(side, :fallback).present?
+
+    render_beacon(side, :main) if default_locale?
+    render_beacon(side, :fallback)
+  end
+
+  private
+
+  def helpscout_beacon_id(side, type = :main)
+    Rails.application.secrets.dig(:helpscout, side, type)
+  end
+
+  def render_beacon(side, type)
+    render partial: "support/beacon", locals: {
+        beacon_id: helpscout_beacon_id(side, type)
+    }
+  end
+
+  def default_locale?
+    current_locale.to_sym == I18n.default_locale
+  end
 end
