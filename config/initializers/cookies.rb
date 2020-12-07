ccbd82314ca091b3d9e99c0b3ded86b2b4a291d6
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-12-07 14:00:07 +0100

    add matomo and jsessionID

---
+++ config/initializers/cookies.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+Rails.application.config.cookies = [
+    {
+        name: "matomo",
+        title: "Matomo",
+        cookies: %w(matomo_session pk_id pk_ses _pk_ref _pk_cvar),
+        purposes: %w(tracking analytics)
+    },
+    {
+        name: "jsessionid",
+        title: "JSESSIONID",
+        cookies: %w(JSESSIONID),
+        purposes: %w(session tracking analytics)
+    }
+]
