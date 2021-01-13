896cd45b95379a1aca54de0c9e32755e572593d1
MoretS <s.moret@ecole-ipssi.net>
2021-01-13 15:13:13 +0100

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -936,13 +936,13 @@ ActiveRecord::Schema.define(version: 2020_11_26_165657) do
     t.string "deepl_api_key"
     t.boolean "force_users_to_authenticate_before_access_organization", default: false
     t.integer "comments_max_length", default: 1000
+    t.jsonb "omniauth_settings"
     t.boolean "rich_text_editor_in_public_views", default: false
     t.jsonb "admin_terms_of_use_body"
     t.string "time_zone", limit: 255, default: "UTC"
     t.boolean "enable_machine_translations", default: false
     t.jsonb "file_upload_settings"
     t.string "machine_translation_display_priority", default: "original", null: false
-    t.jsonb "omniauth_settings"
     t.index ["host"], name: "index_decidim_organizations_on_host", unique: true
     t.index ["name"], name: "index_decidim_organizations_on_name", unique: true
   end
@@ -975,9 +975,9 @@ ActiveRecord::Schema.define(version: 2020_11_26_165657) do
     t.datetime "updated_at", null: false
     t.boolean "active", default: false
     t.integer "position"
+    t.jsonb "action_btn_text"
     t.jsonb "cta_text", default: {}
     t.string "cta_path"
-    t.jsonb "action_btn_text"
     t.index ["decidim_participatory_process_id", "active"], name: "unique_index_to_avoid_duplicate_active_steps", unique: true, where: "(active = true)"
     t.index ["decidim_participatory_process_id", "position"], name: "index_unique_position_for_process", unique: true
     t.index ["decidim_participatory_process_id"], name: "index_decidim_processes_steps__on_decidim_process_id"
@@ -1137,7 +1137,6 @@ ActiveRecord::Schema.define(version: 2020_11_26_165657) do
     t.float "latitude"
     t.float "longitude"
     t.integer "proposal_notes_count", default: 0, null: false
-    t.integer "proposal_endorsements_count", default: 0, null: false
     t.datetime "published_at"
     t.integer "coauthorships_count", default: 0, null: false
     t.integer "position"
