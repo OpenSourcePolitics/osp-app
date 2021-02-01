8f5a6d23bcf46be548490e2a95ff6db75b8fb3fd
MoretS <s.moret@ecole-ipssi.net>
2021-02-01 11:45:47 +0100

    Add Google analytics cookie

--- config/initializers/cookies.rb
+++ config/initializers/cookies.rb
@@ -6,5 +6,12 @@ Rails.application.config.cookies = [
     title: "Matomo",
     cookies: %w(matomo_session pk_id pk_ses _pk_ref _pk_cvar),
     purposes: %w(tracking analytics)
+  },
+
+  {
+    name: "google-analytics",
+    title: "Google Analytics",
+    cookies: %w(_ga _gat _gid __utma __utmb __utmc __utmt __utmz),
+    purposes: %w(tracking analytics)
   }
 ]
